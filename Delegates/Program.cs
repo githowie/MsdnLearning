7d0e3e978f6f03c31aadd2f8bf9952adf0cd259f
howie <misc.howardc@gmail.com>
2017-03-05 13:55:53 -0500

    add more projects

---
+++ Delegates/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        /* Delegate declaration determines the methods that can be referenced by the delegate. 
+         * A delegate can refer to a method, which has the same signature as that of the delegate.
+         * 
+         * Instantiating Delegates
+         * 
+         * Once a delegate type is declared, a delegate object must be created with the new keyword and be associated with a particular method. 
+         * When creating a delegate, the argument passed to the new expression is written similar to a method call, but without the arguments to the method.
+         * 
+         * For example:
+         *  public delegate int NumberChanger(int n);
+         *  ...
+         *  NumberChanger nc1 = new NumberChanger(AddNum);
+         *  NumberChanger nc2 = new NumberChanger(MultNum);
+         */
+
+        // Define the type.
+        public delegate int NumberChanger(int n);
+
+        class TestDelegate
+        {
+            static int num = 10;
+            public static int AddNum(int p)
+            {
+                num += p;
+                return num;
+            }
+
+            public static int MultNum(int q)
+            {
+                num *= q;
+                return num;
+            }
+
+            public static int getNum()
+            {
+                return num;
+            }
+
+            static void Main(string[] args)
+            {
+                //create delegate instances
+                NumberChanger nc;
+                NumberChanger nc1 = new NumberChanger(AddNum);
+                NumberChanger nc2 = new NumberChanger(MultNum);
+                nc = nc1;
+                nc += nc2;
+
+                //calling multicast
+                nc(5);
+                Console.WriteLine("Value of Num: {0}", getNum());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ StaticMember/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StaticMember
+{
+    /* Have a class which you may use without instantiating it, or at least a class where you can use members of it without creating an object for it. 
+     * For instance, you may have a class with a variable that always remains the same, no matter where and how it's used. This is called a static member, static because it remains the same.
+    
+     * A class can be static, and it can have static members, both functions and fields. 
+     * A static class can't be instantiated, so in other words, it will work more as a grouping of related members than an actual class. 
+     * You may choose to create a non-static class instead, but let it have certain static members. 
+     * A non-static class can still be instantiated and used like a regular class, but you can't use a static member on an object of the class. 
+     * A static class may only contain static members.
+    */
+
+    class Program
+    {
+        public static class Rectangle
+        {
+            public static int CalculateArea(int width, int height)
+            {
+                return width * height;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+
+            // To use this method, we call it directly on the class, like this: 
+            Console.WriteLine("The area is: " + Rectangle.CalculateArea(5, 4));
+            Console.ReadKey();
+        }
+    }
+}
